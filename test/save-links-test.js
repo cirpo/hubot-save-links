a86297c873bf06bcf45fe11d77b4721edd8498b1
cirpo <alessandro.cinelli@gmail.com>
2015-07-18 19:32:54 +0400

    testing duplicates

--- test/save-links-test.js
+++ test/save-links-test.js
@@ -1,23 +1,44 @@
 var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
+var path = require('path');
+var Robot = require("hubot/src/robot");
+var TextMessage = require("hubot/src/message").TextMessage;
 
 chai.use(sinonChai);
 var expect = chai.expect;
 
 describe('save-links', function() {
-  var robot = {};
-  var router = {};
-  router.get = sinon.spy();
-  robot.router = router;
-  robot.hear = sinon.spy();
+  var robot;
+  var adapter;
 
-  beforeEach(function(){
-    require('../src')(robot);
+  beforeEach(function(done) {
+    robot = new Robot(null, "mock-adapter", true, "Hubot");
+    robot.adapter.on("connected", function() {
+      require("../index")(robot);
+      adapter = robot.adapter;
+      done();
+    });
+    robot.run();
+
   });
 
-  it('registers a hear listener', function(){
-    expect(robot.hear).to.have.been.calledWith(/[\s\S]*/);
+  it('returns a message if a link was already inserted', function(done) {
+    var link = 'http://example' + (new Date()).getTime() + '.com';
+    adapter.receive(new TextMessage({}, link));
+    adapter.on("send", function(envelope, strings) {
+      try {
+        expect(strings[0]).match(/^#OLD/);
+        done();
+      } catch (err) {
+        done(err)
+      }
+    });
+
+    setTimeout(function(){
+      adapter.receive(new TextMessage({}, link));
+    }, 1000)
+
   })
 
 });
